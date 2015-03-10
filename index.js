b3436fab3e4ababe2065d324184260d96dd89210
joshux <joshux321@gmail.com>
2015-03-10 23:43:05 +0800

    first version, minimatch not yet optimized

---
+++ index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var path = require('path');
+var through = require('through2');
+var minimatch = require('minimatch');
+
+module.exports = function(globs, opt) {
+  if(!isArray(globs))
+    globs = [globs];
+  var cwd = opt.cwd || process.cwd();
+  var stream = through.obj(function(file, enc, cb){
+    var exclude = globs.some(function(glob) {
+      var absGlob = absolute(cwd, glob);
+      return minimatch(file.path, absGlob); // needs to cache optimize
+    });
+    
+    if(!exclude)
+      cb(null, file);
+    else
+      cb(null, null);
+  });
+
+  return stream;
+};
+
+function absolute(cwd, globPattern){
+  var mod = '';
+  if(globPattern[0] === '!') {
+    mod = '!';
+    globPattern = globPattern.slice(1);
+  }
+  return mod + path.resolve(cwd, globPattern);
+}
