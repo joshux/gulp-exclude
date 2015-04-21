a61769e46fbfbc06918fb71e45219d5aefb0362b
joshux <joshux321@gmail.com>
2015-04-21 17:27:43 +0800

    modified test.js

--- test.js
+++ test.js
@@ -3,4 +3,11 @@ var assert = require('assert');
 var gutil = require('gulp-util');
 var exclude = require('./');
 
-describe
+describe('exclude', function(){
+  it('should exclude files', function(cb){
+    //var vStream = gutil.src('**/*.js'); console.log('sfsf');
+    //var piped = vStream.pipe(exclude('exclude-dir/**'));
+    //vStream.on('end', cb);
+    cb();
+  });
+});
